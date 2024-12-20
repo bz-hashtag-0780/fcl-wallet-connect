1d3d5ef0755d0b543518e0bed179604d2d6aae55
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-12-20 11:35:51 +0100

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+// app/layout.tsx
+'use client';
+
+import './globals.css';
+import { AuthContextProvider } from '@/context/AuthContext';
+import '../flow-config';
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body suppressHydrationWarning={true}>
+				<AuthContextProvider>{children}</AuthContextProvider>
+			</body>
+		</html>
+	);
+}
